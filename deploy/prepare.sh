9f43acdae8e6d526486fc274f12735804794a7ab
carey.xu <carey.xuhan@huawei.com>
2015-08-10 23:02:31 -0400

    Add deploy scripts for compass
    JIRA: COMPASS-11
    
    Change-Id: Id588912c73b424228b6f01cd47fd282d040f7b8f
    Signed-off-by: carey.xu <carey.xuhan@huawei.com>

---
+++ deploy/prepare.sh
@@ -0,0 +1,35 @@
+function prepare_env() {
+    export PYTHONPATH=/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/dist-packages
+    sudo apt-get update -y
+    sudo apt-get install mkisofs bc
+    sudo apt-get install git python-pip python-dev -y
+    sudo apt-get install libxslt-dev libxml2-dev libvirt-dev build-essential qemu-utils qemu-kvm libvirt-bin virtinst libmysqld-dev -y
+    sudo pip install --upgrade pip
+    sudo pip install --upgrade ansible
+    sudo pip install --upgrade virtualenv
+    sudo service libvirt-bin restart
+
+    # prepare work dir
+    sudo rm -rf $WORK_DIR
+    mkdir -p $WORK_DIR
+    mkdir -p $WORK_DIR/installer
+    mkdir -p $WORK_DIR/vm
+    mkdir -p $WORK_DIR/network
+    mkdir -p $WORK_DIR/iso
+    mkdir -p $WORK_DIR/venv
+
+    if [[ ! -f centos.iso ]];then
+        wget -O $WORK_DIR/iso/centos.iso $ISO_URL
+    fi
+
+    # copy compass
+    mkdir -p $WORK_DIR/mnt
+    sudo mount -o loop $WORK_DIR/iso/centos.iso $WORK_DIR/mnt
+    cp -rf $WORK_DIR/mnt/compass/compass-core $WORK_DIR/installer/
+    cp -rf $WORK_DIR/mnt/compass/compass-install $WORK_DIR/installer/
+    sudo umount $WORK_DIR/mnt
+    rm -rf $WORK_DIR/mnt
+
+    chmod 755 $WORK_DIR -R
+    virtualenv $WORK_DIR/venv
+}
