177ff58bcffbd9810158dd324aa3f9c880c0a47d
Chigang(Justin) <chigang@huawei.com>
2015-08-13 03:14:03 -0400

    Generate ppa repositories automatically
    Openstack ppa repo will be generated by the patch.now we support
    openstack(juno) on trusty/centos7.1. there are two steps about
    how to generate the repo:
    1. analyze the deployment script to find the deploying packages
    2. use Docker as container to collect packages
    
    JIRA: COMPASS-15
    
    Change-Id: I74ac57b70455643ecf9d32effd44a2e46f404f4e
    Signed-off-by: Chigang(Justin) <chigang@huawei.com>

--- ci/build.sh
+++ ci/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -ex
 SCRIPT_DIR=`cd ${BASH_SOURCE[0]%/*};pwd`
+COMPASS_DIR=`cd ${BASH_SOURCE[0]%/*}/../;pwd`
 WORK_DIR=$SCRIPT_DIR/work
-
 sudo rm -rf $WORK_DIR
 mkdir -p $WORK_DIR
-
+#PACKAGE_URL=${PACKAGE_URL:-http://58.251.166.184/repo}
 COMPASS_CORE=${COMPASS_CORE:-http://github.com/baigk/compass-core.git}
 COMPASS_WEB=${COMPASS_WEB:-http://github.com/baigk/compass-web.git}
 COMPASS_INSTALL=${COMPASS_INSTALL:-http://github.com/baigk/compass-install.git}
-COMPASS_ADAPTERS=${COMPASS_ADAPTERS:-http://github.com/baigk/compass-adapters.git}
+
 PACKAGE_URL=${PACKAGE_URL:-http://192.168.127.11:9999/xh/work/package}
 
 cd $WORK_DIR
@@ -18,10 +18,10 @@ cd $WORK_DIR
 wget -O centos_base.iso $PACKAGE_URL/centos_base.iso
 
 # get ubuntu ppa package
-wget -O ubuntu_ppa.tar.gz $PACKAGE_URL/ubuntu_ppa.tar.gz
+wget -O trusty-juno-ppa.tar.gz $PACKAGE_URL/trusty-juno-ppa.tar.gz
 
 # get ubuntu iso
-wget -O Ubuntu-14.04-x86_64.iso $PACKAGE_URL/Ubuntu-14.04-x86_64.iso
+wget -O ubuntu-14.04.3-server-amd64.iso $PACKAGE_URL/ubuntu-14.04.3-server-amd64.iso
 
 # get centos iso
 wget -O CentOS-7-x86_64-Minimal-1503-01.iso $PACKAGE_URL/CentOS-7-x86_64-Minimal-1503-01.iso
@@ -41,21 +41,27 @@ sudo umount base
 chmod 755 ./new -R
 
 # main process
-mkdir -p new/repos new/compass new/bootstrap new/pip new/guestimg
-cp ubuntu_ppa.tar.gz new/repos
-cp Ubuntu-14.04-x86_64.iso new/repos
+mkdir -p new/repos new/compass new/bootstrap new/pip new/guestimg new/app_packages
+cp trusty-juno-ppa.tar.gz new/repos
+cp ubuntu-14.04.3-server-amd64.iso new/repos
 cp CentOS-7-x86_64-Minimal-1503-01.iso new/repos
 cp centos_7_1_common_ppa_repo.tar.gz new/repos
 cp centos_7_1_openstack_juno_ppa_repo.tar.gz new/repos
 cp loaders.tar.gz new
 wget -O new/guestimg/cirros-0.3.3-x86_64-disk.img $PACKAGE_URL/cirros-0.3.3-x86_64-disk.img
 wget -O new/pip/pexpect-3.3.tar.gz https://pypi.python.org/packages/source/p/pexpect/pexpect-3.3.tar.gz#md5=0de72541d3f1374b795472fed841dce8
+wget -O new/app_packages/packages.tar.gz $PACKAGE_URL/packages.tar.gz
 
 cd new/compass
 git clone ${COMPASS_CORE}
 git clone ${COMPASS_INSTALL}
-git clone ${COMPASS_ADAPTERS}
 git clone ${COMPASS_WEB}
+
+git fetch
+git checkout origin/master -- $COMPASS_DIR/deploy/adapters
+
+cp $COMPASS_DIR/deploy/adapters ./compass-adapters -rf
+
 
 find . -name ".git" |xargs rm -rf
 
@@ -64,5 +70,7 @@ cp -rf $SCRIPT_DIR/ks.cfg new/isolinux/ks.cfg
 rm -rf new/.rr_moved
 sudo mkisofs -quiet -r -J -R -b isolinux/isolinux.bin  -no-emul-boot -boot-load-size 4 -boot-info-table -hide-rr-moved -x "lost+found:" -o compass.iso new/
 
+cp compass.iso $SCRIPT_DIR -f
+
 # delete tmp file
-sudo rm -rf new base ubuntu_ppa.tar.gz centos_base.iso Ubuntu-14.04-x86_64.iso
+sudo rm -rf new base trusty-juno-ppa.tar.gz centos_base.iso ubuntu-14.04.3-server-amd64.iso
