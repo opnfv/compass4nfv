85ffb5818b49b8ee06ef0095744c823958317963
Chigang(Justin) <chigang@huawei.com>
2015-08-13 03:14:03 -0400

    Generate ppa repositories automatically
    Openstack ppa repo will be generated by the patch.now we support
    ubuntu(trusty) and openstack(juno). there are two steps about
    how to generate the repo:
    1. analyze the deployment script to find the deploying packages
    2. use Docker as container to collect packages
    
    JIRA: COMPASS-15
    
    Change-Id: I74ac57b70455643ecf9d32effd44a2e46f404f4e
    Signed-off-by: Chigang(Justin) <chigang@huawei.com>

---
+++ ci/make_repo.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -ex
+
+WORK_PATH=$(cd "$(dirname "$0")"; pwd)
+REPO_PATH=${WORK_PATH}/repo
+UBUNTU_TAG="trusty"
+CENTOS_TAG="centos7"
+OPENSTACK_TAG="juno"
+DOCKER_TAG="${UBUNTU_TAG}/openstack-${OPENSTACK_TAG}"
+DOCKER_FILE=${WORK_PATH}/${UBUNTU_TAG}/${OPENSTACK_TAG}/Dockerfile
+DEPLOY_SCRIPT_PATH="$1"
+#DEPLOY_SCRIPT_PATH="/home/chigang/code/compass4nfv/deploy/adapters/ansible/"
+
+if [[ $UID != 0 ]]; then
+    echo "You are not root user!"
+    exit 1
+fi
+
+if [[ ! -d ${REPO_PATH} ]]; then
+    mkdir -p ${REPO_PATH}
+fi
+
+docker info
+if [[ $? != 0 ]]; then
+    wget -qO- https://get.docker.com/ | sh
+else
+    echo "docker is already installed!"
+fi
+
+if [[ -e ${WORK_PATH}/cp_repo.sh ]]; then
+    rm -f ${WORK_PATH}/cp_repo.sh
+fi
+
+cat <<EOF >${WORK_PATH}/cp_repo.sh
+#!/bin/bash
+set -ex
+cp /*ppa.tar.gz /result
+EOF
+
+if [[ -e ${WORK_PATH}/install_packeages.sh ]]; then
+    rm -f ${WORK_PATH}/install_packages.sh
+fi
+
+#make ubuntu 14.04 ppa
+apt-get install python-yaml -y
+apt-get install python-cheetah -y
+
+python gen_ins_pkg_script.py ${DEPLOY_SCRIPT_PATH} Debian Debian_juno.tmpl
+
+docker build -t ${DOCKER_TAG} -f ${DOCKER_FILE} .
+
+mkdir -p ${REPO_PATH}
+docker run -t -v ${REPO_PATH}:/result ${DOCKER_TAG}
+
+IMAGE_ID=$(docker images|grep ${DOCKER_TAG}|awk '{print $3}')
+docker rmi -f ${IMAGE_ID}
+
+#DOCKER_TAG="${CENTOS_TAG}/openstack-${OPENSTACK_TAG}"
+#DOCKER_FILE=${WORK_PATH}/${CENTOS_TAG}/${OPENSTACK_TAG}/Dockerfile
+
+#docker build -t ${DOCKER_TAG} -f ${DOCKER_FILE} .
+
+#mkdir -p ${REPO_PATH}
+#docker run -t -v ${REPO_PATH}:/result ${DOCKER_TAG}
+
+if [[ -e ${WORK_PATH}/install_packages.sh ]]; then
+    rm -f ${WORK_PATH}/install_packages.sh
+fi
+
+if [[ -e ${WORK_PATH}/cp_repo.sh ]]; then
+    rm -f ${WORK_PATH}/cp_repo.sh
+fi
+
