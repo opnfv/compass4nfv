922b6014db31689b93910b777fd4d151a561c820
baigk <baiguoku@huawei.com>
2015-08-24 02:22:26 -0400

    Build improvement for speedup and using public repo
    JIRA: COMPASS-19
    
    Change-Id: I3736635727ef025d2903b5dbe6c842a6d6b8efd8
    Signed-off-by: baigk <baiguoku@huawei.com>

--- ci/build.sh
+++ ci/build.sh
@@ -1,76 +1,176 @@
 #!/bin/bash
 set -ex
+
 SCRIPT_DIR=`cd ${BASH_SOURCE[0]%/*};pwd`
 COMPASS_DIR=`cd ${BASH_SOURCE[0]%/*}/../;pwd`
-WORK_DIR=$SCRIPT_DIR/work
-sudo rm -rf $WORK_DIR
-mkdir -p $WORK_DIR
-#PACKAGE_URL=${PACKAGE_URL:-http://58.251.166.184/repo}
-COMPASS_CORE=${COMPASS_CORE:-http://github.com/baigk/compass-core.git}
-COMPASS_WEB=${COMPASS_WEB:-http://github.com/baigk/compass-web.git}
-COMPASS_INSTALL=${COMPASS_INSTALL:-http://github.com/baigk/compass-install.git}
+WORK_DIR=$SCRIPT_DIR/work/build
 
-PACKAGE_URL=${PACKAGE_URL:-http://192.168.127.11:9999/xh/work/package}
+source $SCRIPT_DIR/build.conf
+
+mkdir -p $WORK_DIR $WORK_DIR/cache
 
 cd $WORK_DIR
 
-# get base iso
-wget -O centos_base.iso $PACKAGE_URL/centos_base.iso
+function prepare_env()
+{
+    set +e
+    sudo mkisofs -version
+    if [[ $? -ne 0 ]]; then
+        sudo apt-get install genisoimage
+    fi
+    set -e
+}
 
-# get ubuntu ppa package
-wget -O trusty-juno-ppa.tar.gz $PACKAGE_URL/trusty-juno-ppa.tar.gz
+function download_git()
+{
+     if [[ -d $WORK_DIR/cache/${1%.*} ]]; then
+        if [[  -d $WORK_DIR/cache/${1%.*}/.git ]]; then
 
-# get ubuntu iso
-wget -O ubuntu-14.04.3-server-amd64.iso $PACKAGE_URL/ubuntu-14.04.3-server-amd64.iso
+             cd $WORK_DIR/cache/${1%.*}
 
-# get centos iso
-wget -O CentOS-7-x86_64-Minimal-1503-01.iso $PACKAGE_URL/CentOS-7-x86_64-Minimal-1503-01.iso
+             git fetch origin master
+             git checkout origin/master
 
-# get cenos common ppa package
-wget -O centos_7_1_common_ppa_repo.tar.gz $PACKAGE_URL/centos_7_1_common_ppa_repo.tar.gz
+             cd -
 
-# get centos openstack ppa package
-wget -O centos_7_1_openstack_juno_ppa_repo.tar.gz $PACKAGE_URL/centos_7_1_openstack_juno_ppa_repo.tar.gz
+             return
+         fi
 
-wget -O loaders.tar.gz $PACKAGE_URL/loaders.tar.gz
-# mount base iso
-mkdir -p base
-sudo mount -o loop centos_base.iso base
-cd base;find .|cpio -pd ../new;cd -
-sudo umount base
-chmod 755 ./new -R
+         rm -rf $WORK_DIR/cache/${1%.*}
+     fi
 
-# main process
-mkdir -p new/repos new/compass new/bootstrap new/pip new/guestimg new/app_packages
-cp trusty-juno-ppa.tar.gz new/repos
-cp ubuntu-14.04.3-server-amd64.iso new/repos
-cp CentOS-7-x86_64-Minimal-1503-01.iso new/repos
-cp centos_7_1_common_ppa_repo.tar.gz new/repos
-cp centos_7_1_openstack_juno_ppa_repo.tar.gz new/repos
-cp loaders.tar.gz new
-wget -O new/guestimg/cirros-0.3.3-x86_64-disk.img $PACKAGE_URL/cirros-0.3.3-x86_64-disk.img
-wget -O new/pip/pexpect-3.3.tar.gz https://pypi.python.org/packages/source/p/pexpect/pexpect-3.3.tar.gz#md5=0de72541d3f1374b795472fed841dce8
-wget -O new/app_packages/packages.tar.gz $PACKAGE_URL/packages.tar.gz
+     git clone $2 $WORK_DIR/cache/`basename $i | sed 's/.git//g'`
+}
 
-cd new/compass
-git clone ${COMPASS_CORE}
-git clone ${COMPASS_INSTALL}
-git clone ${COMPASS_WEB}
+function download_url()
+{
+    rm -f $WORK_DIR/cache/$1.md5
+    curl --connect-timeout 10 -o $WORK_DIR/cache/$1.md5 $2.md5
+    if [[ -f $WORK_DIR/cache/$1 ]]; then
+        local_md5=`md5sum $WORK_DIR/cache/$1 | cut -d ' ' -f 1`
+        repo_md5=`cat $WORK_DIR/cache/$1.md5 | cut -d ' ' -f 1`
+        if [[ "$local_md5" == "$repo_md5" ]]; then
+            return
+        fi
+    fi
 
-git fetch
-git checkout origin/master -- $COMPASS_DIR/deploy/adapters
+    curl --connect-timeout 10 -o $WORK_DIR/cache/$1 $2
+}
 
-cp $COMPASS_DIR/deploy/adapters ./compass-adapters -rf
+function download_local()
+{
+    cp $2 $WORK_DIR/cache/ -rf
+}
 
+function download_packages()
+{
+     for i in $CENTOS_BASE $COMPASS_CORE $COMPASS_WEB $COMPASS_INSTALL $TRUSTY_JUNO_PPA \
+              $UBUNTU_ISO $CENTOS_ISO $CENTOS_PPA $LOADERS $CIRROS $PEXCEPT $APP_PACKAGE; do
+         name=`basename $i`
+         if [[ ${name##*.} == "git" ]]; then
+             download_git $name $i
+         elif [[ ${i%%:*} == "http" ]]; then
+             download_url $name $i
+         else
+             download_local $name $i
+         fi
 
-find . -name ".git" |xargs rm -rf
+     done
 
-cd $WORK_DIR
-cp -rf $SCRIPT_DIR/ks.cfg new/isolinux/ks.cfg
-rm -rf new/.rr_moved
-sudo mkisofs -quiet -r -J -R -b isolinux/isolinux.bin  -no-emul-boot -boot-load-size 4 -boot-info-table -hide-rr-moved -x "lost+found:" -o compass.iso new/
+     git fetch
+     git checkout origin/master -- $COMPASS_DIR/deploy/adapters
+}
 
-cp compass.iso $SCRIPT_DIR -f
+function copy_file()
+{
+    new=$1
 
-# delete tmp file
-sudo rm -rf new base trusty-juno-ppa.tar.gz centos_base.iso ubuntu-14.04.3-server-amd64.iso
+    # main process
+    mkdir -p new/repos new/compass new/bootstrap new/pip new/guestimg new/app_packages
+
+    cp -rf $SCRIPT_DIR/ks.cfg new/isolinux/ks.cfg
+
+    rm -rf new/.rr_moved
+
+    for i in $TRUSTY_JUNO_PPA $UBUNTU_ISO $CENTOS_ISO $CENTOS_PPA; do
+        cp $WORK_DIR/cache/`basename $i` new/repos/ -rf
+    done
+
+    cp $WORK_DIR/cache/`basename $LOADERS` new/ -rf || exit 1
+    cp $WORK_DIR/cache/`basename $CIRROS` new/guestimg/ -rf || exit 1
+    cp $WORK_DIR/cache/`basename $APP_PACKAGE` new/app_packages/ -rf || exit 1
+    cp $WORK_DIR/cache/`basename $PEXCEPT` new/pip/ -rf || exit 1
+
+    for i in $COMPASS_CORE $COMPASS_INSTALL $COMPASS_WEB; do
+        cp $WORK_DIR/cache/`basename $i | sed 's/.git//g'` new/compass/ -rf
+    done
+
+    cp $COMPASS_DIR/deploy/adapters new/compass/compass-adapters -rf
+
+    find new/compass -name ".git" |xargs rm -rf
+}
+
+function make_iso()
+{
+
+    download_packages
+    cp  $WORK_DIR/cache/centos_base.iso ./ -f
+    # mount base iso
+    mkdir -p base
+    sudo mount -o loop centos_base.iso base
+    cd base;find .|cpio -pd ../new;cd -
+    sudo umount base
+    chmod 755 ./new -R
+
+    copy_file $new
+
+    sudo mkisofs -quiet -r -J -R -b isolinux/isolinux.bin  -no-emul-boot -boot-load-size 4 -boot-info-table -hide-rr-moved -x "lost+found:" -o compass.iso new/
+
+    md5sum compass.iso > compass.iso.md5
+
+    # delete tmp file
+    sudo rm -rf new base centos_base.iso
+}
+
+function copy_iso()
+{
+   if [[ $# -eq 0 ]]; then
+       return
+   fi
+
+   TEMP=`getopt -o d:f: --long iso-dir:,iso-name: -n 'build.sh' -- "$@"`
+
+   if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+   eval set -- "$TEMP"
+
+   dir=""
+   file=""
+
+   while :; do
+       case "$1" in
+           -d|--iso-dir) dir=$2; shift 2;;
+           -f|--iso-name) file=$2; shift 2;;
+           --) shift; break;;
+           *) echo "Internal error!" ; exit 1 ;;
+       esac
+   done
+
+   if [[ $dir == "" ]]; then
+       dir=$WORK_DIR
+   fi
+
+   if [[ $file == "" ]]; then
+       file="compass.iso"
+   fi
+
+   if [[ "$dir/$file" == "$WORK_DIR/compass.iso" ]]; then
+      return
+   fi
+
+   cp $WORK_DIR/compass.iso $dir/$file -f
+}
+
+prepare_env
+make_iso
+copy_iso $*
