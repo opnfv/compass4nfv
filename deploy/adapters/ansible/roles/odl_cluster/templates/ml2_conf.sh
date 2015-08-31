633175ed2c5d0ab339027ba0a542bcdd4d3769c0
chenshuai@huawei.com <chenshuai@huawei.com>
2015-08-30 21:47:49 -0400

    Write ODL ansible script for Ubuntu14.04
    JIRA: COMPASS-23
    
    Change-Id: Iad692c8f77492989e29d0a4862989edea1d48fc5
    Signed-off-by: chenshuai@huawei.com <chenshuai@huawei.com>

--- deploy/adapters/ansible/roles/odl_cluster/templates/ml2_conf.sh
+++ deploy/adapters/ansible/roles/odl_cluster/templates/ml2_conf.sh
@@ -2,5 +2,5 @@ cat <<EOT>> /etc/neutron/plugins/ml2/ml2_conf.ini
 [ml2_odl]
 password = admin
 username = admin
-url = http://{{ hostvars[inventory_hostname]['ansible_' + INTERNAL_INTERFACE].ipv4.address }}:8080/controller/nb/v2/neutron
+url = http://{{ HA_VIP }}:8080/controller/nb/v2/neutron
 EOT
