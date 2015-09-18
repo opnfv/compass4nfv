a34dc2031dfea25013ef013fd678be59c4d13b7b
carey.xu <carey.xuhan@huawei.com>
2015-09-18 14:55:04 +0800

    separate the mgmt vip from public vip, remove HA_VIP
    Change-Id: Iaa877b7ce93ba9c12bc9be6f3bd101779f07ae9c
    JIRA: COMPASS-51

--- deploy/adapters/ansible/roles/odl_cluster/templates/ml2_conf.sh
+++ deploy/adapters/ansible/roles/odl_cluster/templates/ml2_conf.sh
@@ -2,5 +2,5 @@ cat <<EOT>> /etc/neutron/plugins/ml2/ml2_conf.ini
 [ml2_odl]
 password = admin
 username = admin
-url = http://{{ HA_VIP }}:8080/controller/nb/v2/neutron
+url = http://{{ internal_vip.ip }}:8080/controller/nb/v2/neutron
 EOT
