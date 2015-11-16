6eb83a3120043aea5dc3228993db57d197b8fbd7
grakiss <grakiss.wanglei@huawei.com>
2015-11-16 11:07:54 +0800

    using ELK in web-ui for running log
    JIRA: COMPASS-142
      - make repo for elk
    
    Change-Id: I3f0cc25d0485463edeff40f2ded03863c16fe8b5
    Signed-off-by: grakiss <grakiss.wanglei@huawei.com>

---
+++ build/arch/RedHat/make_kibana.sh
@@ -0,0 +1,52 @@
+
+yum -y install wget 
+yum -y install tar
+yum -y install rpm-build
+
+cat << EOF > ~/.rpmmacros
+%_topdir /root/rpmbuild
+EOF
+
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+pushd .
+
+cd ~/rpmbuild/SOURCES
+wget https://download.elastic.co/kibana/kibana/kibana-4.2.0-linux-x64.tar.gz
+
+cat << EOF > ~/rpmbuild/SPECS/kibana.spec
+Name:       kibana
+Version:    4.2.0
+Release:    1
+Vendor:     elasticsearch
+Summary:    GUN kibana x64 
+License:    GPL
+Source:     kibana-4.2.0-linux-x64.tar.gz
+Group:      System Enviroment/Daemons
+URL:        http://www.elasticsearch.co/
+Packager:   test@test.com
+%description
+kibana package
+
+%prep
+tar xvf ../SOURCES/kibana-4.2.0-linux-x64.tar.gz
+
+%install
+cd ../BUILDROOT
+mkdir -p ./kibana-4.2.0-1.x86_64/opt/kibana
+cp -rf ../BUILD/kibana-4.2.0-linux-x64/* ./kibana-4.2.0-1.x86_64/opt/kibana
+
+%clean
+rm -rf ./kibana-4.2.0-linux-x64
+
+%files
+/opt/kibana/
+EOF
+
+cd ~
+rpmbuild -bb rpmbuild/SPECS/kibana.spec
+
+cp -rf rpmbuild/RPMS/* /var/cache/yum/
+
+find /var/cache/yum -name "perl*" | xargs rm -rf
+popd
