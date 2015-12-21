2181f4cdc03f57c84c909ad7b8e2c6b39e37f288
grakiss <grakiss.wanglei@huawei.com>
2015-12-21 09:27:45 +0800

    fix openstack username from ADMIN to admin
    JIRA: COMPASS-209
      - seems typo problem
    
    Change-Id: I1a2d5681d3db3aa2642492436d4463df0fc71ed7
    Signed-off-by: grakiss <grakiss.wanglei@huawei.com>

--- deploy/adapters/ansible/roles/keystone/templates/admin-openrc.sh
+++ deploy/adapters/ansible/roles/keystone/templates/admin-openrc.sh
@@ -2,6 +2,6 @@
 export OS_PASSWORD={{ ADMIN_PASS }}
 export OS_TENANT_NAME=admin
 export OS_AUTH_URL=http://{{ internal_vip.ip }}:35357/v2.0
-export OS_USERNAME=ADMIN
+export OS_USERNAME=admin
 export OS_VOLUME_API_VERSION=2
 
