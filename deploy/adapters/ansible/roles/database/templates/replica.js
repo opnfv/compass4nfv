ec6a90d676878081f97925d60ca7f10e86ae5d67
carey.xu <carey.xuhan@huawei.com>
2015-12-30 14:37:12 +0800

    bugfix: uncomment ceph-deploy
    JIRA: COMPASS-229
    
    Change-Id: I35fe51c5c48838f7a16338459a50807a4c193697
    Signed-off-by: carey.xu <carey.xuhan@huawei.com>

--- deploy/adapters/ansible/roles/database/templates/replica.js
+++ deploy/adapters/ansible/roles/database/templates/replica.js
@@ -1,7 +1,7 @@
 config = { _id:"compass", members:[
 {% for host in haproxy_hosts.values() %}
 {% set pair = '%s:27017' % host %}
-    {_id:{{ loop.index0 }},host:"{{ pair }}"},
+    {_id:{{ loop.index0 }},host:"{{ pair }}",priority:{{ haproxy_hosts|length - loop.index0 }}},
 {% endfor %}
 ]
 };
