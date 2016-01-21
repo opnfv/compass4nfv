49eec1eb9629cb8892b9cfcc448c61d6af4e85f6
carey.xu <carey.xuhan@huawei.com>
2016-01-21 10:22:52 +0800

    bugfix for jumpserver on ubuntu 14.04.3
      - use virtualenv for python programs
    
    Change-Id: Ia3c28ce90f56628b023899061748db34828a6daa
    Signed-off-by: carey.xu <carey.xuhan@huawei.com>

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,4 @@ if [[ -z $DEPLOY_COMPASS && -z $DEPLOY_HOST && -z $REDEPLOY_HOST ]]; then
     export DEPLOY_HOST="true"
 fi
 
-for i in python-cheetah python-yaml; do
-    if [[ `dpkg-query -l $i` == 0 ]]; then
-        continue
-    fi
-    sudo apt-get install -y --force-yes  $i
-done
-
 $COMPASS_DIR/deploy/launch.sh $*
