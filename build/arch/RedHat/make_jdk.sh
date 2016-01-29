5d5232b4427a38370192b50a9b710a947c741cab
carey.xu <carey.xuhan@huawei.com>
2016-01-29 17:01:14 +0800

    add apache2 headers for shell scripts and ansible scripts
    Change-Id: I34b66d76f59f4300c7ce4e7bd9dc7c1eaddfb384
    Signed-off-by: carey.xu <carey.xuhan@huawei.com>

--- build/arch/RedHat/make_jdk.sh
+++ build/arch/RedHat/make_jdk.sh
@@ -1,3 +1,11 @@
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u65-b17/jdk-8u65-linux-x64.rpm
 
 cp -rf jdk-8u65-linux-x64.rpm /var/cache/yum/
