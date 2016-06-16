75b999bdbec116ca3553241e2a00f6792204c065
chenshuai@huawei.com <chenshuai@huawei.com>
2016-06-16 02:37:15 -0400

    openvswitch: upgrade to 2.5.0
    JIRA: COMPASS-421
    
    Change-Id: I80d0b482c093fd00b4ae0fa8a24b399c918e0beb
    Signed-off-by: chenshuai@huawei.com <chenshuai@huawei.com>

--- build/arch/Debian/make_openvswitch-switch.sh
+++ build/arch/Debian/make_openvswitch-switch.sh
@@ -12,13 +12,13 @@ apt-get install -y build-essential fakeroot debhelper \
          autoconf automake bzip2 libssl-dev \
          openssl graphviz python-all procps \
          python-qt4 python-zopeinterface \
-         python-twisted-conch libtool wget
+         python-twisted-conch libtool wget dh-autoreconf
 
 pushd .
 cd /tmp
-wget http://openvswitch.org/releases/openvswitch-2.3.1.tar.gz
-tar -zxvf openvswitch-2.3.1.tar.gz
-cd openvswitch-2.3.1
+wget http://openvswitch.org/releases/openvswitch-2.5.0.tar.gz
+tar -zxvf openvswitch-2.5.0.tar.gz
+cd openvswitch-2.5.0
 DEB_BUILD_OPTIONS='parallel=8 nocheck' fakeroot debian/rules binary
 cd -
 cp -f *.deb /var/cache/apt/archives/
