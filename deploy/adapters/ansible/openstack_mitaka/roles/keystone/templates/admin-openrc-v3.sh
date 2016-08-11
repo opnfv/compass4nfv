3903df7e5faa655cbe46ba83a760e7cf21402b84
QiLiang <liangqi1@huawei.com>
2016-08-12 01:05:29 +0800

    Add heat user domain configuration
    FIX Rally heat create_update_delete_stack test case failure
    
    JIRA: COMPASS-454
    
    Change-Id: Ib77109c6dde0e7042283e5a9e67be858d360a2ef
    Signed-off-by: QiLiang <liangqi1@huawei.com>

---
+++ deploy/adapters/ansible/openstack_mitaka/roles/keystone/templates/admin-openrc-v3.sh
@@ -0,0 +1,19 @@
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+# Verify the Identity Service installation
+export OS_PASSWORD={{ ADMIN_PASS }}
+export OS_TENANT_NAME=admin
+export OS_AUTH_URL=http://{{ internal_vip.ip }}:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_USERNAME=admin
+export OS_VOLUME_API_VERSION=2
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+
+
