6ad77701f30fa6930798da3027a8d8bebbb0f35e
liyuenan <liyuenan@huawei.com>
2016-09-12 12:13:39 +0800

    Add a expansion functionality
    Through modify the virtual_cluster_expansion.yml, include
    host's name and mac, you can decide to how many compute
    nodes you need to add.
    
    And you also need to modify network.yml. Note that external
    subnet's ip_range should be changed as the first 6 IPs are
    already taken by the first deployment.
    
    Edit ``add.sh``, check the environment variable. Note that
    the OS version and OpenStack version should be same as the
    first deployment.
    
    Run ``add.sh``.
    
    JIRA:COMPASS-481
    
    Change-Id: Id85f02518667e0ff80c2475e70856cd30cf1b9b7
    Signed-off-by: liyuenan <liyuenan@huawei.com>

--- deploy/launch.sh
+++ deploy/launch.sh
@@ -30,38 +30,51 @@ source ${COMPASS_DIR}/deploy/compass_vm.sh
 source ${COMPASS_DIR}/deploy/deploy_host.sh
 
 ######################### main process
-print_logo
+if [ $EXPANSION -eq 0 ]
+then
 
-if [[ ! -z $VIRT_NUMBER ]];then
-    tear_down_machines
-fi
+    print_logo
 
-log_info "########## get host mac begin #############"
-machines=`get_host_macs`
-if [[ -z $machines ]]; then
-    log_error "get_host_macs failed"
-    exit 1
-fi
+    if [[ ! -z $VIRT_NUMBER ]];then
+        tear_down_machines
+    fi
 
-export machines
-
-if [[ "$DEPLOY_COMPASS" == "true" ]]; then
-    if ! prepare_env;then
-        echo "prepare_env failed"
+    log_info "########## get host mac begin #############"
+    machines=`get_host_macs`
+    if [[ -z $machines ]]; then
+        log_error "get_host_macs failed"
         exit 1
     fi
 
-    log_info "########## set up network begin #############"
-    if ! create_nets;then
-        log_error "create_nets failed"
+    export machines
+
+    if [[ "$DEPLOY_COMPASS" == "true" ]]; then
+        if ! prepare_env;then
+            echo "prepare_env failed"
+            exit 1
+        fi
+
+        log_info "########## set up network begin #############"
+        if ! create_nets;then
+            log_error "create_nets failed"
+            exit 1
+        fi
+
+        if ! launch_compass;then
+            log_error "launch_compass failed"
+            exit 1
+        fi
+    fi
+else
+    machines=`get_host_macs`
+    if [[ -z $machines ]];then
+        log_error "get_host_macs failed"
         exit 1
     fi
 
-    if ! launch_compass;then
-        log_error "launch_compass failed"
-        exit 1
-    fi
+    log_info "deploy host macs: $machines"
 fi
+
 
 if [[ -z "$REDEPLOY_HOST" || "$REDEPLOY_HOST" == "false" ]]; then
     if ! set_compass_machine; then
