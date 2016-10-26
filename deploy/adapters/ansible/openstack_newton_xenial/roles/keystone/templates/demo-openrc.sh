97606ef87b3e4611b2db051a0f7e9dbe17288818
liyuenan <liyuenan@huawei.com>
2016-10-26 13:55:23 +0800

    Update the API version for Openstack Newton
    Use the "keystone-manage bootstrap" command to instead of admin_token.
    Because the  admin_token is treated as a "shared secret" that can be used
    to bootstrap Keystone through the API. This "token" does not represent a
    user (it has no identity), and carries no explicit authorization (it
    effectively bypasses most authorization checks).
    
    Use the API v3 to instead of API v2.0. Identity API v3 was established to
    introduce namespacing for users and projects by using "domains" as a
    higher-level container for more flexible identity management and fixed a
    security issue in the v2.0 API (bearer tokens appearing in URLs).
    
    JIRA: COMPASS-491
    
    Change-Id: I56182c14b761728c3492b9dd2b05c3b57aa5f35f
    Signed-off-by: liyuenan <liyuenan@huawei.com>

---
+++ deploy/adapters/ansible/openstack_newton_xenial/roles/keystone/templates/demo-openrc.sh
@@ -0,0 +1,17 @@
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_TENANT_NAME=demo
+export OS_PROJECT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD={{ DEMO_PASS }}
+export OS_AUTH_URL=http://{{ internal_vip.ip }}:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
