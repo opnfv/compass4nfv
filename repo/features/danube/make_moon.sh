2cc4f06322d0f8fc78b8dbc3babacc982db0b650
Yifei Xue <xueyifei@huawei.com>
2016-11-02 18:09:35 +0800

    Create make_repo directory and optimize the current repo making related script
    JIRA: COMPASS-496
    
    Change-Id: I662d996382d38a3e9d0de6598dabe403b9d51665
    Signed-off-by: Yifei Xue <xueyifei@huawei.com>

---
+++ repo/features/danube/make_moon.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -ex
+
+TIMEOUT=10
+
+PACKAGE_URL=http://205.177.226.237:9999
+
+for i in moon.tar.gz; do
+    curl --connect-timeout $TIMEOUT -o /$i $PACKAGE_URL/$i
+done
+
+
