541519f10abbfb8afe493a82c3812dc8ac8f0511
liyuenan <liyuenan@huawei.com>
2016-11-30 14:42:29 +0800

    Deploy Newton automatically in CI
    CI can transfer ci_deploy.sh to deploy newton or mitaka_xenial
    automatically.
    
    JIRA: COMPASS-509
    
    Change-Id: I4db4d759e62a3b574ac8decde11ee8cba80069e2
    Signed-off-by: liyuenan <liyuenan@huawei.com>

---
+++ ci/deploy_ci.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+CI_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+
+if [[ $DEPLOY_SCENARIO == os-nosdn-nofeature-ha ]]; then
+    export OS_VERSION=${OS_VERSION:-xenial}
+    export OPENSTACK_VERSION=${OPENSTACK_VERSION:-newton_xenial}
+
+elif [[ $DEPLOY_SCENARIO == os-odl_2-nofeature-ha ]]; then
+    export OS_VERSION=${OS_VERSION:-xenial}
+    export OPENSTACK_VERSION=${OPENSTACK_VERSION:-newton_xenial}
+
+elif [[ $DEPLOY_SCENARIO == os-odl_l2-moon-ha ]]; then
+    export OS_VERSION=${OS_VERSION:-xenial}
+    export OPENSTACK_VERSION=${OPENSTACK_VERSION:-mitaka_xenial}
+
+elif [[ $DEPLOY_SCENARIO == os-ocl-nofeature-ha ]]; then
+    export OS_VERSION=${OS_VERSION:-trusty}
+    export OPENSTACK_VERSION=${OPENSTACK_VERSION:-liberty}
+
+else
+    export OS_VERSION=${OS_VERSION:-trusty}
+    export OPENSTACK_VERSION=${OPENSTACK_VERSION:-mitaka}
+fi
+
+echo "########################################"
+echo 'DEPLOY_SCENARIO='$DEPLOY_SCENARIO
+echo 'OS_VERSION='$OS_VERSION
+echo 'OPENSTACK_VERSION='$OPENSTACK_VERSION
+echo "########################################"
+
+$CI_DIR/../deploy.sh
