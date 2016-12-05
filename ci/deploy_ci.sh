651e3c2c847d2d5270d03a8b74101d55f0068c69
liyuenan <liyuenan@huawei.com>
2016-12-05 11:27:28 +0800

    Deploy some special scenarios automatically in CI
    For daily build or verify build, adjust OS_VERSION and OPENSTACK_VERSION
    value according to OS_VERSION and DEPLOY_SCENARIO pass from CI.
    
    JIRA: COMPASS-509
    
    Change-Id: I54517015185de7c5944f9af2444feb42fc31e028
    Signed-off-by: liyuenan <liyuenan@huawei.com>

---
+++ ci/deploy_ci.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# ROOT_BUILD_CAUSE DEPLOY_SCENARIO COMPASS_OS_VERSION got from CI
+
+CI_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+
+if [[ $ROOT_BUILD_CAUSE == MANUALTRIGGER ]]; then
+    # For manual ci trigger build, directly use the value pass from CI
+    case $DEPLOY_SCENARIO in
+    os-odl_l2-moon-ha)
+        # os-odl_l2-moon-ha scenario supports xenial mitaka only
+        export COMPASS_OS_VERSION=xenial
+        export OPENSTACK_VERSION=mitaka_xenial
+        ;;  
+    os-ocl-nofeature-ha)
+        # os-ocl-nofeature-ha scenario supports liberty only
+        export COMPASS_OS_VERSION=trusty
+        export OPENSTACK_VERSION=liberty
+        ;;  
+    *)  
+        # setup for testing mitaka by default
+        export COMPASS_OS_VERSION=${COMPASS_OS_VERSION:-trusty}
+        export OPENSTACK_VERSION=${OPENSTACK_VERSION:-mitaka}
+        ;;  
+    esac
+
+else
+    # For daily build or verify build, adjust COMPASS_OS_VERSION and OPENSTACK_VERSION
+    # value according to COMPASS_OS_VERSION and DEPLOY_SCENARIO pass from CI
+
+    if [[ $COMPASS_OS_VERSION == centos ]]; then
+        case $DEPLOY_SCENARIO in
+        os-odl_l2-moon-ha)
+            # os-odl_l2-moon-ha scenario supports xenial mitaka only
+            export COMPASS_OS_VERSION=xenial
+            export OPENSTACK_VERSION=mitaka_xenial
+            ;;
+        os-ocl-nofeature-ha)
+            # os-ocl-nofeature-ha scenario supports liberty only
+            export OPENSTACK_VERSION=liberty
+            ;;
+        *)
+            # setup for testing mitaka by default
+            export OPENSTACK_VERSION=${OPENSTACK_VERSION:-mitaka}
+            ;;
+        esac
+
+    else
+        case $DEPLOY_SCENARIO in
+        os-odl_l2-moon-ha)
+            # os-odl_l2-moon-ha scenario supports xenial mitaka only
+            export COMPASS_OS_VERSION=xenial
+            export OPENSTACK_VERSION=mitaka_xenial
+            ;;
+        os-ocl-nofeature-ha)
+            # os-ocl-nofeature-ha scenario supports liberty only
+            export COMPASS_OS_VERSION=trusty
+            export OPENSTACK_VERSION=liberty
+            ;;
+        *)
+            # setup for testing mitaka by default
+            export COMPASS_OS_VERSION=${COMPASS_OS_VERSION:-trusty}
+            export OPENSTACK_VERSION=${OPENSTACK_VERSION:-mitaka}
+            ;;
+        esac
+    fi
+fi
+
+echo "########################################"
+echo 'DEPLOY_SCENARIO='$DEPLOY_SCENARIO
+echo 'COMPASS_OS_VERSION='$COMPASS_OS_VERSION
+echo 'OPENSTACK_VERSION='$OPENSTACK_VERSION
+echo "########################################"
+
+$CI_DIR/../deploy.sh
