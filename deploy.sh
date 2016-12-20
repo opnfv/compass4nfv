1b0229eab181701014d80e594b2619f4c5ba0291
liyuenan <liyuenan@huawei.com>
2016-12-20 16:44:59 +0800

    Change newton_xenial to newton
    JIRA: COMPASS-513
    
    Change-Id: I937a02bcd69f0df1f5d951dd6630ef2e76e8d386
    Signed-off-by: liyuenan <liyuenan@huawei.com>

--- deploy.sh
+++ deploy.sh
@@ -25,23 +25,11 @@
 ######################### The environment for Openstack ######################
 # Ubuntu16.04 Newton
 #export OS_VERSION=xenial
-#export OPENSTACK_VERSION=newton_xenial
+#export OPENSTACK_VERSION=newton
 
-# Ubuntu14.04 Mitaka
-#export OS_VERSION=trusty
-#export OPENSTACK_VERSION=mitaka
-
-# Ubuntu16.04 Mitaka
-#export OS_VERSION=xenial
-#export OPENSTACK_VERSION=mitaka_xenial
-
-# Centos7 Mitaka
+# Centos7 Newton
 #export OS_VERSION=centos7
-#export OPENSTACK_VERSION=mitaka
-
-# Redhat7 OSP9
-#export OS_VERSION=redhat7
-#export OPENSTACK_VERSION=osp9
+#export OPENSTACK_VERSION=newton
 
 ######################### Hardware Deploy Jumpserver PXE NIC ################
 # You need comment out it when virtual deploy.
