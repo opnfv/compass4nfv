d1db06742389faab33e6e1df5d282f0411c76f51
Harry Huang <huangxiangyu5@huawei.com>
2017-01-18 16:47:25 +0800

    Adjust make repo
    Demand:
      1.Compass4nfv master code only support one OPNFV version
        now, no need to keep previous version's repo scripts
      2.Currently java packages are in /var/www/package/onos
        directory which is unreasonable
      3.repo.conf can generally controll all repo creation.
        We can use a variable in repo.conf to stand for each
        package's name to make it convenient during upgrade
    
    Modification:
      1.Remove previous version's directory in compass4nfv/repo
      2.Only leave java and odl in compass4nfv/repo/features.
        Other features can be added when they are ready
      3.Move java packages into /var/www/package/java directory
      4.Use ODL_PKG variable to stand for odl packages
      5.Use ODL package distribution-karaf-0.5.0-Boron.tar.gz
      6.Use python package netwoking-odl-3.2.0
    
    Change-Id: Ifa818e7630ac4435e3bdb7756b83f80f50c24a76
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>

--- repo/features/scripts/download_java.sh
+++ repo/features/scripts/download_java.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##############################################################################
-# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+# Copyright (c) 2016-2017 HUAWEI TECHNOLOGIES CO.,LTD and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -13,6 +13,7 @@ TIMEOUT=10
 
 PACKAGE_URL=http://205.177.226.237:9999
 
-curl --connect-timeout $TIMEOUT -o /odl.tar.gz $PACKAGE_URL/odl.tar.gz
-
-
+for i in REPLACE_JAVA_PKG; do
+    mkdir -p /pkg/java
+    curl --connect-timeout $TIMEOUT -o /pkg/java/$i $PACKAGE_URL/$i
+done
