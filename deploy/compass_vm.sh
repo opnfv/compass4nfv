2b5676bc3c3f8f735a57c1d3443cd62e7d3704fd
qiwei-li-huawei <qiwei.li@huawei.com>
2017-02-10 15:48:38 +0800

    add ntp for compass-core
    JIRA: COMPASS-514
    
    add ntp for compass-core and set up timezone, make timezone
    in compass-core same as where hosts' are
    To set timezone of both compass-core and hosts, edit
        /deploy/conf/compass.conf
        TIMEZONE: Asia/Shanghai
    
    Change-Id: If60623dbff793e56e65c32f80addc5e1d956f3e1
    Signed-off-by: qiwei-li-huawei <qiwei.li@huawei.com>

--- deploy/compass_vm.sh
+++ deploy/compass_vm.sh
@@ -39,6 +39,7 @@ function set_compass_machine() {
 function install_compass() {
     local inventory_file=$compass_vm_dir/inventory.file
     sed -i "s/mgmt_next_ip:.*/mgmt_next_ip: ${COMPASS_SERVER}/g" $WORK_DIR/installer/compass-install/install/group_vars/all
+    sed -i "s/timezone:.*/timezone: ${TIMEZONE}/g" $WORK_DIR/installer/compass-install/install/group_vars/all
     echo "compass_nodocker ansible_ssh_host=$MGMT_IP ansible_ssh_port=22" > $inventory_file
     PYTHONUNBUFFERED=1 ANSIBLE_FORCE_COLOR=true ANSIBLE_HOST_KEY_CHECKING=false ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o ControlMaster=auto -o ControlPersist=60s' ansible-playbook -e pipeline=true --private-key=$rsa_file --user=root --connection=ssh --inventory-file=$inventory_file $WORK_DIR/installer/compass-install/install/$1
     exit_status=$?
