24abda88231c4e9d01ca53f02f9510d27975a0cd
liyuenan <liyuenan@huawei.com>
2017-02-14 09:02:56 +0800

    Support keystone api v2
    JIRA: COMPASS-524
    
    Openstack newton deployed by compass use keystone api v3 defaultly. If you
    want to use keystone api v2, you just need run this command before deploy.
        export KEYSTONE_API=v2.0
    
    Change-Id: I679b46bc2d0d8f35aa4153609fd249e8e1591011
    Signed-off-by: liyuenan <liyuenan@huawei.com>

--- deploy/adapters/ansible/roles/keystone/templates/admin-openrc-v2.sh
+++ deploy/adapters/ansible/roles/keystone/templates/admin-openrc-v2.sh
@@ -8,7 +8,7 @@
 ##############################################################################
 # Verify the Identity Service installation
 export OS_PASSWORD={{ ADMIN_PASS }}
-export OS_TENANT_NAME=admin
+export OS_PROJECT_NAME=admin
 export OS_AUTH_URL=http://{{ internal_vip.ip }}:35357/v2.0
 export OS_USERNAME=admin
 export OS_VOLUME_API_VERSION=2
