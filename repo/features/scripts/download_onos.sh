f6f9e275941e9dd4129afbd09367e38857eb0f0f
rssaravana <saravanakumar.somaskandan@huawei.com>
2017-02-17 16:44:25 +0530

    onos 1.8 upgradation for danube
    Change-Id: I6242b9fc09749ca83f446ef5456eaad72f248012
    Signed-off-by: rssaravana <saravanakumar.somaskandan@huawei.com>
    (cherry picked from commit 8da5888adfc919b20807f0783e1ae841fac4244e)

---
+++ repo/features/scripts/download_onos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -ex
+
+TIMEOUT=10
+
+for i in REPLACE_ONOS_PKG; do
+    mkdir -p /pkg/onos
+    curl --connect-timeout $TIMEOUT -Lo /pkg/onos/${i##*/} $i
+done
