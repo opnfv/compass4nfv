6c98364691f8c91bcdf09fab4cb30af03925a867
QiLiang <liangqi1@huawei.com>
2017-05-08 11:09:02 +0800

    WIP: Update moon role for moonv4
    JIRA: -
    
    Configure moon_master: "Enable" in DHA file to enable moon-master deploy.
    Configure moon_slave: "Enable" in DHA file to enable moon-slave deploy.
    
    Change-Id: Ie300a85979af31634ab0660ccee1ee8b094110b5
    Signed-off-by: QiLiang <liangqi1@huawei.com>

--- deploy/client.py
+++ deploy/client.py
@@ -243,8 +243,11 @@ opts = [
     cfg.StrOpt('odl_l3_agent',
                help='odl l3 agent enable flag',
                default='Disable'),
-    cfg.StrOpt('moon',
-               help='moon enable flag',
+    cfg.StrOpt('moon_master',
+               help='moon master enable flag',
+               default='Disable'),
+    cfg.StrOpt('moon_slave',
+               help='moon slave enable flag',
                default='Disable'),
     cfg.StrOpt('onos_sfc',
                help='onos_sfc enable flag',
@@ -751,12 +754,14 @@ class CompassClient(object):
         package_config['enable_secgroup'] = (CONF.enable_secgroup == "true")
         package_config['enable_fwaas'] = (CONF.enable_fwaas == "true")
         package_config['enable_vpnaas'] = (CONF.enable_vpnaas == "true")
-        package_config[
-            'odl_l3_agent'] = "Enable" if CONF.odl_l3_agent == "Enable" else "Disable"   # noqa
-        package_config[
-            'moon'] = "Enable" if CONF.moon == "Enable" else "Disable"
-        package_config[
-            'onos_sfc'] = "Enable" if CONF.onos_sfc == "Enable" else "Disable"
+        package_config['odl_l3_agent'] = "Enable" \
+            if CONF.odl_l3_agent == "Enable" else "Disable"
+        package_config['moon_master'] = "Enable" \
+            if CONF.moon_master == "Enable" else "Disable"
+        package_config['moon_slave'] = "Enable" \
+            if CONF.moon_slave == "Enable" else "Disable"
+        package_config['onos_sfc'] = "Enable" \
+            if CONF.onos_sfc == "Enable" else "Disable"
 
         status, resp = self.client.update_cluster_config(
             cluster_id, package_config=package_config)
