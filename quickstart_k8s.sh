846e46d5941c683330ed4e55826523508432885d
chigang <chigang@huawei.com>
2017-09-14 14:32:11 +0800

    add kubernetes quickstart
    add a shell script to quickstart kubernetes virtual deployment
    
    Change-Id: I6c76c550b3e65c1e0aa74712aa9a5c46cc022d07
    Signed-off-by: chigang <chigang@huawei.com>

---
+++ quickstart_k8s.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2016-2017 HUAWEI TECHNOLOGIES CO.,LTD and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+sudo apt-get update
+sudo apt-get install -y git
+
+git clone https://gerrit.opnfv.org/gerrit/compass4nfv
+
+pushd compass4nfv
+
+CURRENT_DIR=$PWD
+
+#k8s only support on centos
+export OS_VERSION=centos7
+export KUBERNETES_VERSION="v1.7.3"
+SCENARIO=${SCENARIO:-os-nosdn-nofeature-ha.yml}
+
+./build.sh
+
+export TAR_URL=file://$CURRENT_DIR/work/building/compass.tar.gz
+export DHA=$CURRENT_DIR/deploy/conf/vm_environment/$SCENARIO
+export NETWORK=$CURRENT_DIR/deploy/conf/vm_environment/network.yml
+
+./deploy.sh
