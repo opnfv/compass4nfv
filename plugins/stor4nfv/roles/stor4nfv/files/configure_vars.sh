fe70d9b9b2bd1485b4f208e1e5d29a37639ffa4e
ramamani yeleswarapu <ramamani.yeleswarapu@intel.com>
2018-02-07 21:33:40 -0800

    [WIP] Add plugin for Stor4nfv
    Change-Id: Iee718a48994ad4e55f4b7a01933bd4e7773ac67f
    Signed-off-by: ramamani yeleswarapu <ramamani.yeleswarapu@intel.com>

---
+++ plugins/stor4nfv/roles/stor4nfv/files/configure_vars.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd $HOME/gopath/src/github.com/stor4nfv/stor4nfv/ci/ansible
+
+sed -i 's/^workplace.*/workplace: \/root/g' group_vars/common.yml
+
+sed -i 's/^enabled_backend.*/enabled_backend: ceph/g' group_vars/osdsdock.yml
+
+sed -i 's/^ceph_pool_name.*/ceph_pool_name: "rbd"/g' group_vars/osdsdock.yml
+
+sed -i 's/^ceph_origin.*/ceph_origin: repository/g' group_vars/ceph/all.yml
+
+sed -i 's/^ceph_repository.*/ceph_repository: community/g' group_vars/ceph/all.yml
+
+sed -i 's/^ceph_stable_release.*/ceph_stable_release: luminous/g' group_vars/ceph/all.yml
+
+sed -i 's/^public_network.*/public_network: '"$1"'/g' group_vars/ceph/all.yml
+
+sed -i 's/^cluster_network.*/cluster_network: '"$2"'/g' group_vars/ceph/all.yml
+
+sed -i 's/^monitor_interface.*/monitor_interface: eth0/g' group_vars/ceph/all.yml
+
+sed -i 's/^devices:.*/devices: [\/dev\/loop0, \/dev\/loop1, \/dev\/loop2]/g' group_vars/ceph/osds.yml
+
+sed -i 's/^osd_scenario.*/osd_scenario: collocated/g' group_vars/ceph/osds.yml
+
+sed -i 's/^db_endpoint.*/db_endpoint: localhost:62379,localhost:62380/g' group_vars/osdsdb.yml
+
+sed -i 's/^etcd_port.*/etcd_port: 62379/g' group_vars/osdsdb.yml
+
+sed -i 's/^etcd_peer_port.*/etcd_peer_port: 62380/g' group_vars/osdsdb.yml
