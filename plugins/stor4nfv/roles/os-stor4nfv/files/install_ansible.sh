d28c0255bfc468df993cbb6fd6432f8794dc346d
ramamani yeleswarapu <ramamani.yeleswarapu@intel.com>
2018-08-01 18:23:01 -0700

    [stor4nfv] Add os-stor4nfv role for OpenStack deploy
    Adds os-stor4nfv role for OpenStack deploy which installs
    OpenSDS and Cindercompatibleapi.
    
    Disables Cinder service and configures cindercompatibleapi
    to work with 'lvm' backend.
    
    Adds os-nosdn-stor4nfv-ha scenario file.
    Adds os-nosdn-stor4nfv-noha scenario file.
    
    Change-Id: Ie4b6a874beb3f61aa2fd08ebc6c97ab737c012ee
    Signed-off-by: ramamani yeleswarapu <ramamani.yeleswarapu@intel.com>

---
+++ plugins/stor4nfv/roles/os-stor4nfv/files/install_ansible.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# #############################################################################
+# Copyright (c) 2018 Intel Corp.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+# #############################################################################
+
+cd /opt
+
+add-apt-repository ppa:ansible/ansible-2.4
+
+apt-get update
+apt-get -f install -y ansible
+sleep 5
+
+ansible --version
+
