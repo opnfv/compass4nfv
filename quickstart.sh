ca9955b1ec7f22a90cb27e6768e6d709cd9bddc6
Yibo Cai <yibo.cai@arm.com>
2018-09-03 12:55:39 +0800

    update quickstart_k8s.sh to support arm64
    Change-Id: Ibb4839c34f86f701c2938fafd32b2669b06235aa
    Signed-off-by: Yibo Cai <yibo.cai@arm.com>

--- quickstart.sh
+++ quickstart.sh
@@ -7,6 +7,13 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+
+COMPASS_ARCH=$(uname -m)
+if [ "$COMPASS_ARCH" = "aarch64" ]; then
+    echo "Not support aarch64, please try quickstart_k8s.sh instead"
+    exit 1
+fi
+
 sudo apt-get update
 sudo apt-get install -y git
 
